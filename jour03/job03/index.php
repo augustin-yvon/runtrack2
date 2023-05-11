7ca6f4000c0ffaf9902e800a3dcf593b8f46be9b
augustin-yvon <115154566+augustin-yvon@users.noreply.github.com>
2023-05-11 11:32:50 +0200

    rendu job03

---
+++ jour03/job03/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $str = "I'm sorry Dave I'm afraid I can't do that.";
+    $voyelle = ["a","e","i","o","u","y"];
+    for ($i = 0; $i < strlen($str); $i++ ) {
+        if (in_array(strtolower($str[$i]), $voyelle)) {
+            print($str[$i]);
+        }
+    }
+    ?>
+</body>
+</html>
