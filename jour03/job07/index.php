d77deb265047dd55c852a586b12aa29c1b029d06
augustin-yvon <115154566+augustin-yvon@users.noreply.github.com>
2023-05-11 14:11:24 +0200

    rendu job07

---
+++ jour03/job07/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $str = "Certaines choses changent, et d'autres ne changeront jamais.";
+    $premierCaractere = $str[0];
+    
+    for ($i = 0; $i < strlen($str) - 1; $i++) {
+        $str[$i] = $str[$i + 1];
+    }
+    
+    $str[-1] = $premierCaractere;
+    
+    print($str)
+    ?>
+</body>
+</html>
