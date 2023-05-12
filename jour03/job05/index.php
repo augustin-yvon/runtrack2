ad71b5d8da3fed0281c377cdde8768b10c8936ea
augustin-yvon <115154566+augustin-yvon@users.noreply.github.com>
2023-05-12 12:51:53 +0200

    Update index.php
    Remplacement de toutes les fonction système par des algorithmes

--- jour03/job05/index.php
+++ jour03/job05/index.php
@@ -8,24 +8,45 @@
 </head>
 <body>
     <?php 
-    $str = "On n'est pas le meilleur quand on le croit mais quand on le sait.";
+    $str = "On n'est pas le meilleur quand on le croit mais quand on le sait";
 
-    $voyelle = ["a","e","i","o","u","y"];
-    $car_exclue = [" ", ",", ".", "'", ":", "/", "!"];
+    $voyelles = ["a","e","i","o","u","y","A","E","I","O","U","Y"];
+    $consonnes = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","z","B","C","D","F","G","H","J","K","L","M","N","O","P","Q","R","S","T","V","W","X","Z"];
+    
+    $len = 0;
+    while (isset($str[$len])) {
+        $len++;
+    }
 
     $compteur_voyelles = 0;
     $compteur_consonnes = 0;
 
-    for ($i = 0; $i < strlen($str); $i++ ) {
-        
-        if (in_array(strtolower($str[$i]), $voyelle)) {
-            $compteur_voyelles++;
+    for ($i = 0; $i < $len-1; $i++ ) {
 
-        }elseif (in_array($str[$i], $car_exclue)) {
-            continue;
+        for ($i = 0; $i < $len; $i++ ) {
+            $is_voyelle = false;
+            foreach($voyelles as $v){
+                if($str[$i] == $v){
+                    $is_voyelle = true;
+                    break;
+                }
+            }
+            if ($is_voyelle) {
+                $compteur_voyelles++;
+            }
+        }
 
-        }else {
-            $compteur_consonnes++;
+        for ($i = 0; $i < $len-1; $i++ ) {
+            $is_consonne = false;
+            foreach($consonnes as $c){
+                if($str[$i] == $c){
+                    $is_consonne = true;
+                    break;
+                }
+            }
+            if ($is_consonne) {
+                $compteur_consonnes++;
+            }
         }
     }
 
