41ba44243f03afad72bd81ca15cda17c1570010c
augustin-yvon <115154566+augustin-yvon@users.noreply.github.com>
2023-05-12 12:08:08 +0200

    Update index.php

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -9,7 +9,13 @@
 <body>
     <?php 
     $str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.";
-    for ($i = 0; $i < strlen($str); $i += 2) {
+    
+    $count = 0;
+    while (isset($str[$count])) {
+        $count++;
+    }
+
+    for ($i = 0; $i < $count; $i += 2) {
         print($str[$i]);
     }
     ?>
